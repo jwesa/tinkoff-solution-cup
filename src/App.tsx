74276c007b6d21fadf8a973abd7fa866a738d5c3
jwesa <layoshicity@gmail.com>
2023-04-22 17:28:39 +0300

    Убрал ненужные utils и добавил роут

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { History } from "./pages/history";
 import { AppLayout } from "./app-layout";
 import { AddCosts } from "./pages/add-costs";
@@ -9,6 +9,7 @@ export const App = observer(() => {
   return (
     <AppLayout>
       <Routes>
+        <Route path={"*"} element={<Navigate to={"/add_costs"} />} />
         <Route path={"history"} element={<History />} />
         <Route path={"add_costs"} element={<AddCosts />} />
       </Routes>
--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { killNulls } from "./utils";
